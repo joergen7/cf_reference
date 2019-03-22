f50847ca9a9c743f8527039a8c8d1fe15180ef77
Jorgen Brandt <joergen@cuneiform-lang.org>
2019-03-22 02:16:20 +0100

    email address changed

--- cfl-types.rkt
+++ cfl-types.rkt
@@ -3,7 +3,7 @@
 ;; Semantic reference of the Cuneiform distributed programming
 ;; language
 ;;
-;; Copyright 2016-2019 Jörgen Brandt <joergen.brandt@onlinehome.de>
+;; Copyright 2016-2019 Jörgen Brandt <joergen@cuneiform-lang.org>
 ;;
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
